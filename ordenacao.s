3b100861d073e7c9a3f4f63adbdedeafee67ebe5
ViniDias <90332822+ViniDias1@users.noreply.github.com>
2022-09-15 21:49:27 -0300

    Update ordenacao.s

--- ordenacao.s
+++ ordenacao.s
@@ -5,40 +5,39 @@
 		bun main
 		.align 5
         
-     // r2 = aux
-     // r3 = iterador i
-     // r4 = iterador k
-     
-     
      lerIN:
-     	// r17 = IN
+     	// r16 -> Iterador
+		// r17 -> IN
      	mov r8, vetor
+        mov r16, 0
+     	cmpi r16, 399
+        beq 5
         l8 r17, [r1] 
-        s32 [r8], r17
-        addi r8,r8,4
+        s8 [r8], r17
+        addi r8,r8,1
+        addi r16,r16,1
+        bun -7
 		ret
      
-     // Funcao ordem normal dos numeros    
+    // Funcao ordem normal dos numeros    
     imprimirORDEMNORMAL: 
     	mov r8, vetor 
-        l32 r10, [r8]  
+        l8 r10, [r8]  
         cmpi r10, 0        
         beq 3       
-        s32 [r1], r10      
-        addi r8, r8, 4  
+        s8 [r1], r10      
+        addi r8, r8, 1
         bun -6        
         ret
         
-        
     // Funcao Bubble sort
-    
     bbSort:
-    	cmpi r3, 19
+    	cmpi r3, 399
         mov r7, vetor
         mov r8, vetor
         mov r9, vetor
         beq 20
-        cmpi r4, 19
+        cmpi r4, 399
         beq 15
         l32 r10,[r8]   
 		addi r8, r8, 4 
@@ -59,27 +58,30 @@
         mov r4, 0
         bun -25
         ret
+        
     imprimirBBSORT:
     	mov r8, vetor 
-        l32 r10, [r8]  
+        l8 r10, [r8]  
         cmpi r10, 0        
         beq 3       
-        s32 [r1], r10      
-        addi r8, r8, 4  
+        s8 [r1], r10      
+        addi r8, r8, 1 
         bun -6        
         ret
    
 	// Função principal
 	main:
+    
     	mov sp, 0x7FFC
         l32 r1, [terminal]
-        mov r2,0
+        mov r2, 0
         mov r3, 0
         mov r4, 0
+        
         //leitura do IN para o TERMINAL
-        //call lerIN
+        call lerIN
         //ler o numeros do vetor na ordem normal
-        //call imprimirORDEMNORMAL
+        call imprimirORDEMNORMAL
         //ordena o vetor
         call bbSort
         //ler o vetor ordenado
@@ -89,12 +91,11 @@
 		int 0
 
 .data
-	
     terminal:
     	.4byte 0x8888888B
     vetor:
-    	//.fill 400
-    .4byte 0x0000000A,0x00000009,0x00000008,0x00000007,0x00000006,0x00000005,0x00000004,0x00000003,0x00000002,0x00000001,0x000000E3,0x000000F2,0x0000000A2,0x0000000D9,0x0000000B1,0x000000C7,0x000000F5,0x000000A1,0x000000B4,0x000000F6
+    	.fill 400
+    
 
 
         
